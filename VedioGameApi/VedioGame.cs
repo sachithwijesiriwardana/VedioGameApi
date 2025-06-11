e71e86fbb2f46c0de7bc0ec1128d8f8e7651b3e1
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-06-11 21:07:14 +0530

    Add project files.

---
+++ VedioGameApi/VedioGame.cs
@@ -0,0 +1,12 @@
+﻿namespace VedioGameApi
+{
+    public class VedioGame
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Platform { get; set; }
+        public string? Developer { get; set; }
+        public string? Publisher { get; set; }
+    }
+}
